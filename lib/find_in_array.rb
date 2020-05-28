3968e2132669358e0e8b8b2278623eb9b94f5f22
Roman Opalacz <roman9891@gmail.com>
2020-05-28 01:16:20 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,7 +1,10 @@
 def find_element_index(array, value_to_find)
   i = 0 
   while array[i] do 
-    if array[i] == value_to_find {return i}
+    if array[i] == value_to_find 
+      return i
+    end
+    i += 1
   end
   nil
 end
